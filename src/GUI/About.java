5508d41fea66442d5619f0040c1198da01283a8a
Sebastian <!!@localhost>
2011-09-21 19:31:14 -0300

    mensajes de confirmacion de guardar en bd

--- src/GUI/About.java
+++ src/GUI/About.java
@@ -3,13 +3,18 @@ package GUI;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
-public class About extends JPanel {
+import utiles.Constantes;
+
+public class About extends JPanelBackground {
     private JLabel labelTitle = new JLabel();
     private JLabel labelAuthor = new JLabel();
     private GridBagLayout layoutMain = new GridBagLayout();
@@ -24,12 +29,31 @@ public class About extends JPanel {
     }
 
     private void jbInit() throws Exception {
-        this.setLayout( layoutMain );
+        
+    	Imagenes misImagenes = new Imagenes();
+    	
+    	try {
+    		this.setBackground(misImagenes.getImagenURL(Constantes.FONDO_3));
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		ImageIcon imagenAprender;
+		try {
+			imagenAprender = new ImageIcon(misImagenes.getImagenURL(Constantes.IMAGEN_APRENDER));
+			JLabel etiqueta = new JLabel(imagenAprender);
+			this.add(etiqueta);
+		} catch (MalformedURLException e) {
+			System.err.println(e.getMessage());
+		}
+    	
+    	this.setLayout( layoutMain );
         this.setBorder( border );
+        
         labelTitle.setText("Homerock");
         labelAuthor.setText("Autores : SaPazos & Yamil3g");
-        this.add( labelTitle, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE,
-                    new Insets(5, 15, 0, 15), 0, 0));
-        this.add( labelAuthor, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 15, 0, 15), 0, 0) );
+       // this.add( labelTitle, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE,
+       //            new Insets(5, 15, 0, 15), 0, 0));
+       // this.add( labelAuthor, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 15, 0, 15), 0, 0) );
     }
 }
