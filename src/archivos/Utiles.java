883c326892ace9c5665ae656cc0e8bbf2e26b8b6
Sebastian <!!@localhost>
2011-10-10 19:03:10 -0300

    bug en repeats

--- src/archivos/Utiles.java
+++ src/archivos/Utiles.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.StringTokenizer;
+
+import excepciones.ArchivosException;
 import utiles.*;
 
 public class Utiles {
@@ -18,6 +20,7 @@ public class Utiles {
 	private static String[] notasPpales= {"A","B","C","D","E","F","G"};
 	private static ArrayList<String> notasPosibles=new ArrayList(Arrays.asList(notasPpales));
 
+	private static boolean DEBUG = false;
 
 	/*################################################################################################################
   ###################						 FUNCIONES 						###################################### 
@@ -41,10 +44,11 @@ public class Utiles {
 	 * @param	ArrayList con la cancion y sus repeats
 	 * @param soloAcordes: indica si solo guardaremos acordes (true), o todo 
 	 * @return ArrayList con la cancion sin los repeats o null en caso de error
+	 * @throws ArchivosException 
 	 *  
 	 **/
 	//#########################################################################################
-	public static ArrayList<String> quitarRepets(ArrayList<String> cancion, boolean soloAcordes){
+	public static ArrayList<String> quitarRepets(ArrayList<String> cancion, boolean soloAcordes) throws ArchivosException{
 
 		ArrayList<Integer> arrayRepeat=new ArrayList<Integer>();
 		ArrayList<Integer> arrayRepeatEnding=new ArrayList<Integer>();
@@ -151,118 +155,23 @@ public class Utiles {
 
 			}//fin del while
 		}catch(ArrayIndexOutOfBoundsException e) {
-			System.out.print(cancion.toString());
-			System.out.println("error de indice de array \n" );
-			System.out.println(Traza);
-			e.printStackTrace();
-			return null;
+			if (DEBUG) {
+				System.out.print(cancion.toString());
+				System.out.println("error de indice de array \n" );
+				System.out.println(Traza);
+				e.printStackTrace();
+			}
+			throw new ArchivosException("Error en la definicion de repeats - Archivo mal formado");
+			
 		}
 		return listaDeNotas;
 	}
 
-	//#########################################################################################
 	/**
-	 * obtenerDuracion
-	 * Obtiene la duracion de una cancion (cantidad de compases) analizando la lista cancionConEstilos (con repeats)
-	 * @param cancion con estilos
-	 * @return la duracion de la cancion
-	 **/
-	//#########################################################################################
-	/*
-	public static int obtenerDuracion(ArrayList<String> cancion){
-
-		ArrayList<Integer> arrayRepeat=new ArrayList<Integer>();
-		ArrayList<Integer> arrayRepeatEnding=new ArrayList<Integer>();
-		ArrayList<Integer> arrayRepeatEnd=new ArrayList<Integer>();
-		ArrayList<Integer> arrayRepeticiones=new ArrayList<Integer>();
-
-		int cont = 0;
-		int pos = 0;
-		int cantRepeticiones = 0;
-		ArrayList<String> listaDeNotas=new ArrayList<String>();
-		String valor="";
-
-		try {
-			
-			while(pos< cancion.size()){
-				valor =cancion.get(pos);
-				
-				if (!valor.startsWith(REPEAT) && !valor.startsWith(ESTILO)) {
-					cont = cont+1;
-				}
-
-				//si es repeat ending
-				if (valor.indexOf(REPEAT_ENDING)!=-1){
-
-					if (!arrayRepeatEnding.contains(pos)) {
-						arrayRepeatEnding.add(pos);
-						arrayRepeticiones.add(calcRepeticiones(valor));
-					}
-
-					// si llegamos a cero
-					if (arrayRepeticiones.get(arrayRepeticiones.size()-1)==0){
-						pos=arrayRepeatEnd.get(arrayRepeatEnd.size()-1);
-						arrayRepeticiones.remove(arrayRepeticiones.size()-1);
-						arrayRepeatEnd.remove(arrayRepeatEnd.size()-1);
-						arrayRepeat.remove(arrayRepeat.size()-1);
-						arrayRepeatEnding.remove(arrayRepeatEnding.size()-1);
-					}
-
-				}else {
-					// si es repeatEnd
-					if(valor.indexOf(REPEAT_END)!=-1){
-
-						if (!arrayRepeatEnd.contains(pos)) {
-							arrayRepeatEnd.add(pos);
-							int posRepeat=0;
-							int posRepeatEnding=0;
-							//si el ARRAY REPEATENDING NO ES VACIO
-							if (!arrayRepeatEnding.isEmpty()){
-								posRepeatEnding=arrayRepeatEnding.get(arrayRepeatEnding.size()-1);
-							}
-
-							posRepeat = arrayRepeat.get(arrayRepeat.size()-1); 
-							//si no tengo un repeatEnding para el ultimo repeat
-							// debe estar entre repeat y repeatEnd , sino es asi es de otro bucle
-							if(posRepeatEnding<=posRepeat | posRepeatEnding>pos ){
-								arrayRepeticiones.add(calcRepeticiones(valor));
-							}
-						}
-						if (!arrayRepeticiones.isEmpty() && arrayRepeticiones.get(arrayRepeticiones.size()-1)>0){
-							cantRepeticiones = arrayRepeticiones.get(arrayRepeticiones.size()-1);
-							pos=arrayRepeat.get(arrayRepeat.size()-1);
-							cantRepeticiones = cantRepeticiones - 1;
-							arrayRepeticiones.set(arrayRepeticiones.size()-1, cantRepeticiones);
-						}	else {
-
-							if (!arrayRepeticiones.isEmpty()){
-								arrayRepeticiones.remove(arrayRepeticiones.size()-1);
-							}
-							arrayRepeatEnd.remove(arrayRepeatEnd.size()-1);
-							arrayRepeat.remove(arrayRepeat.size()-1);
-						}
-					}else {
-						//si es Repeat
-						if (valor.indexOf(REPEAT)!=-1){
-							if (!arrayRepeat.contains(pos)) {
-								arrayRepeat.add(pos);
-							}
-						}
-					}
-				}
-				pos++;
-
-			}//fin del while
-		}catch(ArrayIndexOutOfBoundsException e) {
-			System.out.print(cancion.toString());
-			System.out.println("error de indice de array \n" );
-			e.printStackTrace();
-			return 0;
-		}
-		return cont;
-	}
-*/
-	
+	 * 	
+	 * @param cancionSinRepeats
+	 * @return
+	 */
 	public static int obtenerDuracion(ArrayList<String> cancionSinRepeats){
 		int contador=0;
 		
