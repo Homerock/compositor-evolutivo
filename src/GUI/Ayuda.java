b7d217cc3abac588f5a86e101a1a2e4c78432c5b
Yamil <compositor.evolutivo@gmail.com>
2011-10-18 19:30:35 -0300

    modificando la ayuda de homerock

--- src/GUI/Ayuda.java
+++ src/GUI/Ayuda.java
@@ -59,46 +59,50 @@ public class Ayuda extends JPanelBackground {
         	jTextAreaAyuda = new JTextArea();
         	jTextAreaAyuda.setEditable(false);
         	jTextAreaAyuda.setText(
-        			"Homerock es un sistema de composición musical, aprende\n" +
-        			"canciones que le permiten componer. \n" +
-        			"Podés guardar tus composiciones y volver a verlas, \n" +
-        			"reproducirlas o modificarlas. \n\n" +
-
-        			"Homerock tiene cuatro funciones principales: \n\n" +
-
-        			"Aprender: \n" +
-        			"Para que Homerock aprenda canciones seleccionamos \n" +
-        			"el directorio o archivo que queremos que aprenda.  \n" +
-        			"Los archivos deben tener el formato mma. \n" +
-        			"así, con cada canción que aprende, Homerock incrementa  \n" +
-        			"sus conocimientos, recordá que cuanto más y mejores \n" +
-        			"canciones aprenda obtendrá mejores resultados. \n\n" +
-
-        			"Componer: \n" +
-        			"Para componer Homerock tiene que haber aprendido  \n" +
-        			"canciones previamente, en base a sus conocimientos \n" +
-        			"podés seleccionar un estilo y un acorde de tónica \n" +
-        			"para generar una composición. \n" +
-        			"También podés utilizar estructuras de estrofas y/o \n" +
-        			"elegir el tempo y duración de la canción. \n" +
-
-        			"Cuando se compone una canción Homerock la reproduce \n" +
-        			"y muestra el detalle de la canción, separada en estrofas\n" +
-        			"y compases. \n" +
-        			"Esto te permite modificar uno o varios compases de \n" +
-        			"la canción. \n" +
-        			"Cada composición se puede guardar para volver a verla \n" +
-        			"o escucharla en cualquier momento. \n\n" +
-
-        			"Mis canciones: \n" +
-        			"Todas las composiciones que guardaste las podés volver \n" +
-        			"a ver, reproducir o modificar para volver a guardar \n" +
-        			"una nueva versión de cada canción. \n\n" +
-
-        			"Guardar: \n" +
-        			"Podés guardar en la base de datos todo lo que homerock \n" +
-        			"aprendió y así recuperar todo el conocimiento la próxima \n" +
-        			"vez que utilices Homerock.");
+        			"Homerock es un sistema de composición musical,\n"+
+        			"es decir su funcion principal es componer canciones\n"+
+        			"de manera inteligente.Simula el aprendizaje de un \n"+
+        			"compositor aprendiendo canciones que se le ingresan, \n"+
+        			"cuanto mas canciones aprenda mas conocimientos tendra \n"+
+        			"y mejores seran las composiciones obtenidas.\n"+
+        			"\n"+
+        			"\n"+
+        			"Homerock tiene cuatro funciones principales: \n"+
+        			"\n"+
+        			"Aprender: \n"+
+        			"Para que Homerock aprenda canciones seleccionamos \n"+
+        			"el directorio o archivo que queremos que aprenda.  \n"+
+        			"Los archivos deben tener el formato mma.\n"+
+        			"\n"+
+        			"Componer: \n"+
+        			"Para componer Homerock tiene que haber aprendido  \n"+
+        			"canciones previamente, seleccionas un estilo \n"+
+        			"y un acorde de tónica para generar una composición. \n"+
+        			"También podés utilizar estructuras de estrofas y/o \n"+
+        			"elegir el tempo y duración de la canción. \n"+
+        			"Cuando se compone una canción Homerock la reproduce y \n"+
+        			"muestra el detalle de la canción, separada en estrofas y \n"+
+        			"compases en el panel de la derecha. \n"+
+        			"Es posible modificar uno o varios compases de \n"+
+        			"la canción, marcando el compas a modificar y \n"+
+        			"luego el boton 'Modificar Cancion'. \n"+
+        			"Al momento de reproducir la cancion se pueden \n"+
+        			"visualizar las pistas de la cancion, \n"+
+        			"silenciar alguna de ellas o modificar \n"+
+        			"el tempo de la cancion, con el boton 'Opciones Avanzadas' .\n"+
+        			"Cada composición se puede guardar para volver a verla \n"+
+        			"o escucharla en cualquier momento. \n"+
+        			"\n"+
+        			"Mis canciones: \n"+
+        			"Todas las composiciones que guardaste las podés volver \n"+
+        			"a ver, reproducir o modificar para volver a guardar \n"+
+        			"una nueva versión de cada canción. \n"+
+        			"\n"+
+        			"Guardar: \n"+
+        			"Podés guardar en la base de datos todo lo que homerock \n"+
+        			"aprendió y así recuperar todo el conocimiento la próxima \n"+
+        			"vez que utilices Homerock.\n"		
+        	);
         	
         	JScrollPane scroll = new JScrollPane(jTextAreaAyuda);
         	this.add(scroll);
